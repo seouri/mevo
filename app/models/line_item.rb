3c105787de594085f6341ce1b874cafb2277c3f1
Joon Lee <joon@kimlee.org>
2008-09-10 10:22:12 -0400

    added tree_number search to LineItem

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,12 @@
 class LineItem < ActiveRecord::Base
   belongs_to :book, :counter_cache => true
   belongs_to :term, :counter_cache => true
+
+  def self.search(query, options = {})
+    options[:conditions] ||= ["#{LineItem.table_name}.tree_number LIKE ?", "#{query}%"] unless query.blank?
+    options[:page]      ||= 1
+    options[:per_page]  ||= 15
+    options.delete_if {|k,v| k == :total_entries and v.nil?}
+    paginate options
+  end
 end
