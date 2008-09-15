169cc368d821d590c6facfd62325e859fbcd145a
Joon Lee <joon@kimlee.org>
2008-09-15 08:27:24 -0400

    show years to terms index

--- app/models/term.rb
+++ app/models/term.rb
@@ -5,7 +5,7 @@ class Term < ActiveRecord::Base
   def self.search(query, options = {})
     options[:conditions] ||= ["#{Term.table_name}.term LIKE ?", "#{query}%"] unless query.blank?
     options[:page]      ||= 1
-    options[:per_page]  ||= 20
+    options[:per_page]  ||= 10
     options.delete_if {|k,v| k == :total_entries and v.nil?}
     paginate options
   end
