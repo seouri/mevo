da43ee8b1ee2f955b9ce01e34882b55ec7de8130
Joon Lee <seouri@gmail.com>
2009-04-27 15:45:46 -0400

    refactored ancestors and children nodes to Term model

--- app/helpers/terms_helper.rb
+++ app/helpers/terms_helper.rb
@@ -1,11 +1,2 @@
 module TermsHelper
-  def ancestors_and_children(term)
-    unique_nodes = []
-    term.nodes.each do |node|
-      unique_nodes.push(node.ancestors)
-      unique_nodes.push(node)
-      unique_nodes.push(node.children)
-    end
-    unique_nodes.flatten.uniq
-  end
 end
--- app/models/term.rb
+++ app/models/term.rb
@@ -9,4 +9,14 @@ class Term < ActiveRecord::Base
     options.delete_if {|k,v| k == :total_entries and v.nil?}
     paginate options
   end
+  
+  def ancestors_and_children
+    unique_nodes = []
+    nodes.each do |node|
+      unique_nodes.push(node.ancestors)
+      unique_nodes.push(node)
+      unique_nodes.push(node.children)
+    end
+    unique_nodes.flatten.uniq
+  end
 end
